d6db175794e5cc844e75fcdbfbf339376489613d
Bruno Marques <brunoa879@gmail.com>
2021-10-13 22:10:32 -0300

    segundo commit

---
+++ ExibirPares/src/ExibirPares.java
@@ -0,0 +1,12 @@
+
+public class ExibirPares {
+	public static void main(String[] args) {
+		int valor;
+		for (int i = 0; i < 100; i++) {
+			if (i % 2 == 0) {
+				System.out.println("i =" + i);
+			}
+		}
+	}
+
+}
